5991ab0dc21a1111fa70a4b5e4e856f3add6c3b6
Сергей <seregabur@live.ru>
2022-04-02 08:42:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace Двумерный_массив
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int startRandom = 0;
+            int stopRandom = 10;
+            int[,] twoMassiv = new int[5,6];
+            int u = twoMassiv.Rank;
+
+            for (int i = 0; i < twoMassiv.GetLength(0);i++)
+            {
+                for(int ii=0;ii<twoMassiv.GetLength(1);ii++)
+                {
+                    twoMassiv[i, ii] = randomNumber.Next(startRandom, stopRandom);
+                }
+            }
+
+            for (int i = 0; i < twoMassiv.GetLength(0); i++)
+            {
+                for (int ii = 0; ii < twoMassiv.GetLength(1); ii++)
+                {
+                    Console.Write(twoMassiv[i,ii]+" ");
+                }
+
+                Console.Write("\n");
+            }
+            Console.WriteLine(u);
+           // Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
