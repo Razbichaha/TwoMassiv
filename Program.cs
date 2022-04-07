9c28f558137b47ddb3e197f3bb16ff5069b13123
Сергей <seregabur@live.ru>
2022-04-07 13:49:34 +0300

    2

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ namespace Двумерный_массив
             Random randomNumber = new Random();
             int startRandom = 0;
             int stopRandom = 10;
-            int[,] twoMassiv = new int[5,6];
+            int Line = 4;
+            int column = 3;
+            int[,] twoMassiv = new int[Line,column];
 
             for (int i = 0; i < twoMassiv.GetLength(0);i++)
             {
@@ -32,6 +34,24 @@ namespace Двумерный_массив
                 Console.Write("\n");
             }
 
+            int temp = 0;
+            int stringPointer = 1;
+
+            for(int i=0;i< twoMassiv.GetLength(stringPointer);i++)
+            {
+                temp += twoMassiv[stringPointer,i];
+            }
+
+            Console.Write("Сумма второй строки = " + temp + "\n");
+            int columnPointer = 0;
+            temp = 1;
+
+            for (int i = 0; i < twoMassiv.GetLength(columnPointer); i++)//\\\\\\\\\\\\\\\\\\
+            {
+                temp *= twoMassiv[i, columnPointer];
+            }
+
+            Console.Write("Произведение первого столбца = " + temp + "\n");
             Console.ReadLine();
         }
     }
